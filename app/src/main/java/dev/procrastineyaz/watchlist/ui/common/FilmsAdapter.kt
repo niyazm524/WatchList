8b8c8333b97e8c452543411753fe75b52e204f13
niyazm524 <niyazm524@gmail.com>
2020-04-28 18:24:43 +0300

    profile fragment layout and recycler added

---
+++ app/src/main/java/dev/procrastineyaz/watchlist/ui/common/FilmsAdapter.kt
@@ -0,0 +1,53 @@
+package dev.procrastineyaz.watchlist.ui.common
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import dev.procrastineyaz.watchlist.R
+import dev.procrastineyaz.watchlist.ui.common.data.Movie
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_movie.*
+
+class FilmsAdapter : ListAdapter<Movie, MovieViewHolder>(MoviesDiffCallback()) {
+
+    init {
+        setHasStableIds(true)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return MovieViewHolder(inflater.inflate(R.layout.item_movie, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        holder.bindTo(getItem(position))
+    }
+
+    override fun getItemId(position: Int) = getItem(position).id
+
+}
+
+
+class MovieViewHolder(override val containerView: View) :
+    RecyclerView.ViewHolder(containerView), LayoutContainer {
+
+    fun bindTo(movie: Movie) {
+        tv_title.text = movie.title
+        tv_title_global.text = movie.titleGlobal
+        tv_rating.text = movie.rating.toString()
+        tv_note.text = "\uD83D\uDCDD\t" + movie.note
+        Glide
+            .with(containerView)
+            .load(movie.posterUri)
+            .into(iv_poster)
+    }
+}
+
+class MoviesDiffCallback : DiffUtil.ItemCallback<Movie>() {
+    override fun areItemsTheSame(oldItem: Movie, newItem: Movie) = oldItem.id == newItem.id
+    override fun areContentsTheSame(oldItem: Movie, newItem: Movie) = oldItem == newItem
+}
--- app/src/main/java/dev/procrastineyaz/watchlist/ui/feed/FeedFragment.kt
+++ app/src/main/java/dev/procrastineyaz/watchlist/ui/feed/FeedFragment.kt
@@ -1,31 +1,59 @@
 package dev.procrastineyaz.watchlist.ui.feed
 
+import android.content.ContentResolver
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.annotation.DrawableRes
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import dev.procrastineyaz.watchlist.R
+import dev.procrastineyaz.watchlist.ui.common.FilmsAdapter
+import dev.procrastineyaz.watchlist.ui.common.data.Movie
+import kotlinx.android.synthetic.main.fragment_feed.*
 
 class FeedFragment : Fragment() {
 
     private lateinit var dashboardViewModel: FeedViewModel
+    private val filmsAdapter = FilmsAdapter()
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         dashboardViewModel =
-                ViewModelProviders.of(this).get(FeedViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+            ViewModelProviders.of(this).get(FeedViewModel::class.java)
+
+        return inflater.inflate(R.layout.fragment_feed, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        rv_movies.apply {
+            adapter = filmsAdapter
+        }
+        filmsAdapter.submitList(
+            listOf(
+                Movie(
+                    id = 1,
+                    title = "Остров проклятых",
+                    titleGlobal = "Shutter island (2009)",
+                    rating = 9.3f,
+                    note = "ДиКаприо сходит с ума на острове",
+                    posterUri = getUriOfDrawable(R.drawable.film_1).toString()
+                )
+            )
+        )
+    }
+
+    private fun getUriOfDrawable(@DrawableRes drawableId: Int): Uri = Uri.Builder()
+        .scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
+        .authority(resources.getResourcePackageName(drawableId))
+        .appendPath(resources.getResourceTypeName(drawableId))
+        .appendPath(resources.getResourceEntryName(drawableId))
+        .build()
+
 }

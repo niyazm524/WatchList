55751b8fc33e12ac0254aa5391d7494e2d49a49d
niyazm524 <niyazm524@gmail.com>
2020-09-27 12:25:54 +0300

    bottom navigation retain state hack

--- app/src/main/java/dev/procrastineyaz/watchlist/ui/main/MainActivity.kt
+++ app/src/main/java/dev/procrastineyaz/watchlist/ui/main/MainActivity.kt
@@ -2,25 +2,50 @@ package dev.procrastineyaz.watchlist.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dev.procrastineyaz.watchlist.R
+import dev.procrastineyaz.watchlist.ui.helpers.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var navController: NavController
+    private var currentNavController: LiveData<NavController>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        if (savedInstanceState == null) {
+            setupBottomNavigationBar()
+        } // Else, need to wait for onRestoreInstanceState
 
-        navController = findNavController(R.id.nav_host_fragment)
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_feed, R.id.navigation_trends, R.id.navigation_more))
-        navView.setupWithNavController(navController)
+    }
 
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        // Now that BottomNavigationBar has restored its instance state
+        // and its selectedItemId, we can proceed with setting up the
+        // BottomNavigationBar with Navigation
+        setupBottomNavigationBar()
+    }
+
+    private fun setupBottomNavigationBar() {
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.nav_view)
+        val navGraphIds = listOf(
+                R.navigation.nav_home, R.navigation.nav_feed, R.navigation.nav_trends, R.navigation.nav_more
+        )
+
+        // Setup the bottom navigation view with a list of navigation graphs
+        val controller = bottomNavigationView.setupWithNavController(
+            navGraphIds = navGraphIds,
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.nav_host_fragment,
+            intent = intent
+        )
+
+        currentNavController = controller
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return currentNavController?.value?.navigateUp() ?: false
     }
 }
